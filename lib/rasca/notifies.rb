0427d9dc45af6d20a24f69804527c1948945a58a
Miguel Armas <kuko@canarytek.com>
2011-11-06 11:45:35 +0000

    Improvements and tests on notifications

--- lib/rasca/notifies.rb
+++ lib/rasca/notifies.rb
@@ -8,15 +8,19 @@ module Rasca
 # syslog
 
 # Generic notification class
-# On creation we have to pass a Hash of methods and destinations
-# The destination depends on the notification type:
-# { :print => nil, :nsca => "nsca_server"}
+# On creation we need to pass these parameters:
+# name: The name of the Notification (Alert)
+# client: The client machine that is originating this alert
+# methods: a Hash of methods and destinations
+#    The destination depends on the notification type:
+#    { :print => nil, :nsca => "nsca_server"}
 #
 class Notification
-  attr_accessor :long, :short
+  attr_accessor :long, :short, :name, :client
   attr_reader :notifications
 
-  def initialize(name,methods)
+  # 
+  def initialize(name,client,methods)
     # Short message (1 line)
     @short = ""
     # Long message (multiline)
@@ -29,16 +33,16 @@ class Notification
       puts "#{type.to_s} -> #{dest}"
       case type
         when :print
-          @notifications.push(NotifyPrint.new(name))
+          @notifications.push(NotifyPrint.new(name,client))
         when :nsca
-          @notifications.push(NotifyNSCA.new(name,dest))
+          @notifications.push(NotifyNSCA.new(name,client,dest))
         else
           raise "Unknown notyfication method: #{type.to_s}"
       end
     end
 
     # Send notifications using all methods
-    def send
+    def notify
       @notifications.each do |method|
         method.notify(@short,@long)
       end
@@ -48,24 +52,49 @@ class Notification
 
 end
 
-# Notifify with NSCA
+# Notify with NSCA
+# parameters
 class NotifyNSCA
-  attr_accessor :server
-  def initialize(name,server)
+  
+  # Mapping between RASCA status and Nagios retcode
+  NAGIOS_RETCODE = { "OK" => 0, "CORRECTED" => 0, "WARNING" => 1, "CRITICAL" => 2, "UNKNOWN" => 3}
+
+  attr_accessor :server, :client, :nsca_path, :nsca_conf
+  def initialize(name,client,server,nsca_path="/usr/bin/send_nsca",nsca_conf="/etc/modularit/send_nsca.cfg")
     @name=name
+    @client=client
     @server=server
+    @nsca_path=nsca_path
+    @nsca_conf=nsca_conf
   end
-  def notify(short,long)
-    puts "nsca #{server} #{short}"
+  def notify(status,short,long)
+    # FIXME: Convert status to nagios RETCODE
+    retcode=1
+    puts "cmd: #{@nsca_cmd} -H #{@server} -c #{@nsca_conf}"
+    puts "msg: #{@client}\t#{@name}\t#{retcode}\t#{short}"
+  end
+  def nsca_cmd
+    "#{@nsca_path} -H #{@server} -c #{@nsca_conf}"
+  end
+  def retcode(status)
+    NAGIOS_RETCODE[status] 
+  end
+  def notify_msg(status,short,long)
+    "#{@client}\t#{@name}\t#{retcode(status)}\t#{short}"
   end
 end
 
 # Just print message
 class NotifyPrint 
-  def initialize(name)
+  attr_accessor :name, :client
+  def initialize(name,client)
     @name=name
+    @client=client
   end
-  def notify(short,long)
+  def notify(status,short,long)
+    puts @client
+    puts @name+": "+status+" "+short
+    puts "--"
     puts long
   end
 end
--- test/test_notification.rb
+++ test/test_notification.rb
@@ -4,7 +4,7 @@ class TestNotification < Test::Unit::TestCase
 
   context "Notifications class" do
     setup do
-      @notification = Rasca::Notification.new("Test",{:print => "", :nsca => "nscaserver.mydomain.com"})
+      @notification = Rasca::Notification.new("Test","modularit.test",{:print => "", :nsca => "nscaserver.mydomain.com"})
     end
 
     should 'initialize messages correctly' do
@@ -37,13 +37,42 @@ class TestNotification < Test::Unit::TestCase
 
     should "raise exception if invalid method" do
       assert_raise RuntimeError do
-        @notification = Rasca::Notification.new("Test",{:print => "", :none => nil})
+        @notification = Rasca::Notification.new("Test","modularit.test",{:print => "", :none => nil})
       end
     end
 
     should "correctly initialize notification objects" do
       assert_respond_to @notification.notifications[0],:notify
       assert_respond_to @notification.notifications[1],:notify
+    end
+
+  end
+
+  context "NotifyNSCA class" do
+    setup do
+      @notify = Rasca::NotifyNSCA.new("TestNSCA","modularit.test","nscaserver.mydomain.com")
+    end
+
+    should "correctly initialize nsca_cmd when no path specified" do
+      assert_equal "/usr/bin/send_nsca -H nscaserver.mydomain.com -c /etc/modularit/send_nsca.cfg", @notify.nsca_cmd
+    end
+
+    should "correctly initialize nsca_cmd when path and config are specified" do
+      @notify.nsca_path="/usr/local/bin/send_nsca"
+      @notify.nsca_conf="/etc/nagios/send_nsca.cfg"
+      assert_equal "/usr/local/bin/send_nsca -H nscaserver.mydomain.com -c /etc/nagios/send_nsca.cfg", @notify.nsca_cmd
+    end
+
+    should "correctly map status to nagios retcodes" do
+      assert_equal 0,@notify.retcode("OK")
+      assert_equal 0,@notify.retcode("CORRECTED")
+      assert_equal 1,@notify.retcode("WARNING")
+      assert_equal 2,@notify.retcode("CRITICAL")
+      assert_equal 3,@notify.retcode("UNKNOWN")
+    end
+
+    should "correctly generate the notify_msg" do
+      assert_equal "modularit.test\tTestNSCA\t0\tOK: It works",@notify.notify_msg("OK","OK: It works",nil)
     end
 
   end
