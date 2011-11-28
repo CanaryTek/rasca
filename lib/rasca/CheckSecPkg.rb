b7ef15092296be91ab2ae13e08444086399082eb
Miguel Armas <kuko@canarytek.com>
2011-11-28 00:10:37 +0000

    fixes to CheckSecg

--- lib/rasca/CheckSecPkg.rb
+++ lib/rasca/CheckSecPkg.rb
@@ -5,12 +5,22 @@ module Rasca
 #   The goal is to make sure that at least all network services are up to date
 # - Optionally, can check updates for additional packages
 class CheckSecPkg < Check
-  attr_accessor :ports_cmd
+  attr_accessor :ports_cmd, :openPorts, :check_update_cmd, :packageList, :packagesToUpdate
   def initialize(*args)
     super
 
+    # Instance variables
+    # Ports in LISTEN state
+    @openPorts=Array.new
+    # Packages containing the processes LISTENing on ports
+    @packageList=Array.new
+    # Packages that needs to be updated
+    @packagesToUpdate=Array.new
+
     # Initialize config variables
     @ports_cmd=@config_values.has_key?(:ports_cmd) ? @config_values[:ports_cmd] : "/usr/sbin/lsof -n -i -P"
+    @check_update_cmd=@config_values.has_key?(:check_update_cmd) ? @config_values[:check_update_cmd] : "yum check-update"
+    @update_cmd=@config_values.has_key?(:update_cmd) ? @config_values[:update_cmd] : "yum update -y"
     
     # More initialization
     #
@@ -25,25 +35,47 @@ class CheckSecPkg < Check
       # Use REAL input
     end
 
-    ## CHECK CODE 
-    ports=getOpenPorts
+    # Initialize status
+    incstatus("OK")
 
-    ## Check updates
-    ports.each do |port|
-      puts "Checking: "+port[:proc] if @debug
-      if @objects.has_key?port[:proc]
-        
-      else
-        incstatus("CRITICAL")
-        @short+="Unknown port #{port[:proto]}/#{port[:port]}, "
-        @long+="Unknown port #{port[:proto]}/#{port[:port]}\n"
-      end
-      
+    ## get ports in LISTENing state
+    getOpenPorts
+
+    # Report unknown ports
+    getUnknownPorts.each do |port|
+      incstatus("CRITICAL")
+      @short+="Unknown port #{port[:proc]} #{port[:proto]}/#{port[:port]}, "
+      @long+="Unknown port #{port[:proc]} #{port[:proto]}/#{port[:port]}\n"
     end
+
+    @packageList=getPackageList
+
+    @packagesToUpdate=getPackagesToUpdate
+
+    if @proactive and not @packagesToUpdate.empty?
+	cmd=@update_cmd+" "+@packagesToUpdate.join(" ")
+	if @verbose
+		puts "Updating packages. Running: "+cmd
+	else
+		cmd+=">/dev/null 2>&1"
+	end
+	puts cmd if @debug
+	if system(cmd)
+            setstatus("CORRECTED")
+            @short+="pending packages UPDATED"
+            @long+="All pending packages UPDATED\n"
+        else
+            incstatus("WARNING")
+            @short+="could not update packages"
+            @long+="Could not update packages\n"
+        end
+    end
+
+    puts @short if @debug
 
     # Set Messages if OK and empty messages
     if status == "OK" and @short == ""
-      @short="Everything OK"
+      @short="All packages up to date"
     end
   end
   # Get the list of open ports
@@ -59,16 +91,73 @@ class CheckSecPkg < Check
       column=line.split(/\s+/)
       entry[:proc]=column[0]
       if column[column.length-1] =~ /LISTEN/
-        entry[:port]=column[column.length-2]
+        entry[:port]=column[column.length-2].split(/:/)[1]
         entry[:proto]=column[column.length-3]
       else
-        entry[:port]=column[column.length-1]
+        entry[:port]=column[column.length-1].split(/:/)[1]
         entry[:proto]=column[column.length-2]
       end
       ports.push(entry)
     end
     puts "Open ports: " if @debug
     puts YAML.dump(ports) if @debug
+    @openPorts=ports
+  end
+  # Find out the packages for the listening services
+  def getPackageList
+    packages=Array.new
+    seen_packages=Array.new
+    @openPorts.each do |port|
+      puts "Checking: "+port[:proc] if @debug
+      if @objects.has_key?port[:proc]
+        if @objects[port[:proc]].has_key? :package
+          package = @objects[port[:proc]][:package]
+          unless seen_packages.include? package
+            seen_packages.push(package)
+            packages.push(package)
+	        end
+        end
+      end
+    end
+    puts YAML.dump(packages) if @debug
+    packages 
+  end
+  # Find out the packages that needs to be updated
+  def getPackagesToUpdate
+    packages=Array.new
+    @packageList.each do |package|
+      # Check if there is an update available
+      if system("#{@check_update_cmd} "+package)
+            incstatus("OK")
+            @long+="Package #{package} up to date\n"
+      else
+            incstatus("WARNING")
+            @short+="#{package} needs to be updated,"
+            @long+="Package #{package} needs to be updated\n"
+            packages.push(package)
+      end
+    end
+    puts YAML.dump(packages) if @debug
+    packages 
+  end
+ 
+  # return a list of unknown open ports (not in object files)
+  def getUnknownPorts
+    ports=Array.new
+    @openPorts.each do |entry|
+      cmd=entry[:proc]
+      port=entry[:port]
+      proto=entry[:proto]
+       
+      if @objects.has_key? cmd
+        # Skip of ports contains ANY
+        next if @objects[cmd][:ports].member? "ANY"
+        # Add port if it is not in process' portlist
+        ports.push(entry) unless @objects[cmd][:ports].member? "#{proto}/#{port}"
+      else
+        ports.push(entry)
+      end
+    end
     ports
   end
   def info
@@ -83,17 +172,27 @@ Check critical package updates
 == Parameters in config file
 
   :none: It doesn't use any additional parameter
+  :ports_cmd: Command to list open ports and associated commands. Default: "/usr/sbin/lsof -n -i -P"
+  :check_update_cmd: Command to check if there are updates for a given package. The package is added after the command.
+			Default: "yum check-update"
+  :update_cmd: Command to update a package. The package is added after the command. Default: "yum update -y"
 
 == Objects format
 
-  none:
-    :option1: 
+  program:   Name of the running program with LISTENing ports
+   :package: Package that contains the program (to check for updates). If no package is given, it only checks for open ports, not for updates
+   :ports: Array containing ports that can be opened by that program. If it contains ANY, that program can open ANY port
 
 Example:
 
-none:
-  :option: Option1
-
+portmap:
+  :package: portmap
+  :ports: [ UDP/111, TCP/111 ]
+rpc.statd:
+  :package: nfs-utils
+  :ports: [ ANY ]
+zimbra:
+  :ports: [ ANY ]
 ]    
   end
 end
--- test/test_CheckSecPkg.rb
+++ test/test_CheckSecPkg.rb
@@ -3,23 +3,163 @@ require "helper"
 class TestCheckSecPkg < Test::Unit::TestCase
 
   context "An instance of CheckSecPkg" do
-
     setup do
       @check=Rasca::CheckSecPkg.new("CheckSecPkg","test/etc",true,true)
-    end
-
-    should 'create open ports list' do
-      result=[ {:proc => "ntpd", :port => "*:123", :proto => "UDP"},
-              {:proc => "sshd", :port => "*:22", :proto => "TCP"}
-              ]
       @check.testing=true
       @check.ports_cmd="cat test/CheckSecPkg/lsof_simple.txt"
       @check.object_dir="test/objects"
       @check.readObjects("CheckSecPkg")
+      @check.check_update_cmd="/bin/false"
       @check.check
-      assert_equal result, @check.getOpenPorts
+    end
+
+    should 'create open ports list' do
+      result=[ {:proc => "ntpd", :port => "123", :proto => "UDP"},
+              {:proc => "sshd", :port => "22", :proto => "TCP"},
+              ]
+      assert_equal result, @check.openPorts
+    end
+
+    should 'create list of packages' do
+      result=[ {:proc => "ntpd", :port => "123", :proto => "UDP"},
+              {:proc => "sshd", :port => "22", :proto => "TCP"},
+              ]
+      assert_equal ["ntp","openssh-server"], @check.packageList
     end
 
   end
+
+  context "CheckSecPkg with unknown processes" do
+    setup do
+      @check=Rasca::CheckSecPkg.new("CheckSecPkg","test/etc",true,true)
+      @check.testing=true
+      @check.ports_cmd="cat test/CheckSecPkg/lsof_unknown.txt"
+      @check.object_dir="test/objects"
+      @check.readObjects("CheckSecPkg")
+      @check.check_update_cmd="/bin/false"
+      @check.check
+    end
+
+    should 'set status=CRITICAL when unknown open port found' do
+      assert_equal "CRITICAL",@check.status
+    end
+
+    should 'return unknown ports' do
+      result = [{:proc => "telnet", :port => "23", :proto => "TCP"}]
+      assert_equal result,@check.getUnknownPorts
+    end
+
+  end
+
+  context "CheckSecPkg with NO packages to update" do
+    setup do
+      @check=Rasca::CheckSecPkg.new("CheckSecPkg","test/etc",true,true)
+      @check.testing=true
+      @check.ports_cmd="cat test/CheckSecPkg/lsof_simple.txt"
+      @check.object_dir="test/objects"
+      @check.readObjects("CheckSecPkg")
+      @check.check_update_cmd="/bin/true"
+      @check.check
+    end
+
+    should 'set status=OK when NO packages need update' do
+      assert_equal "OK",@check.status
+    end
+
+    should 'return empty package list to update' do
+      assert_equal [],@check.packagesToUpdate
+    end
+
+  end
+
+  context "CheckSecPkg with known processes but not matching ports" do
+    setup do
+      @check=Rasca::CheckSecPkg.new("CheckSecPkg","test/etc",true,true)
+      @check.testing=true
+      @check.ports_cmd="cat test/CheckSecPkg/lsof_non_matching_ports.txt"
+      @check.object_dir="test/objects"
+      @check.readObjects("CheckSecPkg")
+      @check.check_update_cmd="/bin/false"
+      @check.check
+    end
+
+    should 'set status=CRITICAL when unknown open port found' do
+      assert_equal "CRITICAL",@check.status
+    end
+
+    should 'return unknown ports' do
+      result = [{:proc => "sshd", :port => "10022", :proto => "TCP"}]
+      assert_equal result,@check.getUnknownPorts
+    end
+
+  end
+
+   context "CheckSecPkg with a known process with ANY in ports" do
+    setup do
+      @check=Rasca::CheckSecPkg.new("CheckSecPkg","test/etc",true,true)
+      @check.testing=true
+      @check.ports_cmd="cat test/CheckSecPkg/lsof_any_ports.txt"
+      @check.object_dir="test/objects"
+      @check.readObjects("CheckSecPkg")
+      @check.check_update_cmd="/bin/false"
+      @check.check
+    end
+
+    should 'set status=OK when the known process listens on ANY port' do
+      assert_equal "WARNING",@check.status
+    end
+
+    should 'not mark the port as unknown' do
+      assert_equal [],@check.getUnknownPorts
+    end
+
+  end
+
+  context "CheckSecPkg with packages to update" do
+    setup do
+      @check=Rasca::CheckSecPkg.new("CheckSecPkg","test/etc",true,true)
+      @check.testing=true
+      @check.ports_cmd="cat test/CheckSecPkg/lsof_simple.txt"
+      @check.object_dir="test/objects"
+      @check.readObjects("CheckSecPkg")
+      @check.check_update_cmd="/bin/false"
+      @check.check
+    end
+
+    should 'set status=WARNING when packages need update' do
+      assert_equal "WARNING",@check.status
+    end
+
+    should 'return packages to update' do
+      assert_equal ["ntp","openssh-server"],@check.packagesToUpdate
+    end
+  end
+
+  context "CheckSecPkg with no package a ANY ports" do
+    setup do
+      @check=Rasca::CheckSecPkg.new("CheckSecPkg","test/etc",true,true)
+      @check.testing=true
+      @check.ports_cmd="cat test/CheckSecPkg/lsof_no_packages.txt"
+      @check.object_dir="test/objects"
+      @check.readObjects("CheckSecPkg")
+      @check.check_update_cmd="/bin/false"
+      @check.check
+    end
+
+    should 'set status=OK and NOT check for updates on that package' do
+      assert_equal "OK",@check.status
+    end
+
+    should 'return packages to update' do
+      assert_equal [],@check.packagesToUpdate
+    end
+
+    should 'not mark the port as unknown' do
+      assert_equal [],@check.getUnknownPorts
+    end
+  end
+
+
+  # detect 
 
 end
