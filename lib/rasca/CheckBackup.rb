35a14a9525a9392882ed973e042aff4953fb741e
Miguel Armas <kuko@canarytek.com>
2012-09-02 23:49:42 +0100

    Check filesystems backups if CheckBackups

--- lib/rasca/CheckBackup.rb
+++ lib/rasca/CheckBackup.rb
@@ -2,13 +2,16 @@ module Rasca
 
 # A Simple Template
 class CheckBackup < Check
+  attr_accessor :testing1, :testing2
+
   def initialize(*args)
     super
 
     # Initialize config variables
-    @log_dir=@config_values.has_key?(:log_dir) ? @config_values[:log_dir] : "/var/lib/pica/obj/lastbackups"
+    @log_dir=@config_values.has_key?(:log_dir) ? @config_values[:log_dir] : "/var/lib/modularit/data/lastbackups"
     @default_expiration=@config_values.has_key?(:default_expiration) ? @config_values[:default_expiration] : 4*24*60*60
     @backup_skip_all=@config_values.has_key?(:backup_skip_all) ? @config_values[:backup_skip_all] : nil
+    @skip_fs_regex=@config_values.has_key?(:skip_fs_regex) ? @config_values[:skip_fs_regex] : /^Filesystem|^tmpfs|^rootfs|^devtmpfs/
 
     # More initialization
     #
@@ -25,93 +28,118 @@ class CheckBackup < Check
     ## Not skipped, we check backups
     @objects=readObjects(@name)
 
-    puts "Objects"
-    puts YAML.dump(@objects)
-        
-    out=""
-    if @testing
-      # Use testing input (for unit testing)
-      out=@testing.split("\n")
-    else
-      # Use REAL input
-      # FIXME: We only check LVM volumes
-      out=`lvscan | grep -v swap | grep -v snap_`
-    end
+    # Create logdir if it doesn't exist
+    FileUtils.mkdir_p @log_dir unless File.directory? @log_dir
+
+    ## Initialize to OK since we may skip everything
+    incstatus("OK")
 
     ## CHECK CODE 
-    # Start setting status=OK because we may not have enything to check
-    incstatus("OK")
-    out.each do |line|
-      expiration=@default_expiration
-      skip=false
-      line.chomp!
-      if line =~ /^.*'(.*)'.*$/
-        lv = File.basename($1)
-        puts "LV: "+lv if @debug
-        # Fixme: Check for skip
-        if @objects.has_key? lv
-          # Ok, we have an entry, If entry is a hash, read parameters
-          if @objects[lv].instance_of?Hash 
-            expiration=@objects[lv][:expiration] if @objects[lv].has_key? :expiration
-          else
-            # If it's not a has, just skip
-            puts "Skipping #{lv}" if @debug
-            skip=true
-          end
-        end
-        unless skip
-          if File.exist? "#{@log_dir}/#{lv}"
-            puts "logfile for #{lv} exists, good!" if @debug
-            mtime=File.stat("#{@log_dir}/#{lv}").mtime
-            puts "mtime : "+mtime.to_s if @debug
-            if (Time.now - mtime) > expiration
-              puts "OLD backup of #{lv}" if @debug
-              @short+="OLD bcklog for #{lv},"
-              @long+="Backup of #{lv} id too old"
-              incstatus("WARNING")
-            else
-              incstatus("OK")
-            end
-          else
-            puts "  WARNING: no logfile for #{lv}\n" if @debug
-            @short+="no bcklog for #{lv},"
-            @long+="Never seen a backup of #{lv}\n"
-            incstatus("WARNING")
-          end
-        end
-      end
-    end
+    check_lv
+    check_fs
 
     # Set Messages if OK and empty messages
     if status == "OK" and @short == ""
       @short="Everything OK"
     end
   end
+
+  ## Checks found LVs for backups
+  def check_lv
+    out=""
+    if @testing1
+      # Use testing input (for unit testing)
+      out=@testing1.split("\n")
+    else
+      # Use REAL input
+      out=`lvscan | grep -v swap | grep -v snap_`
+    end
+    out.each do |line|
+      line.chomp!
+      if line =~ /^.*'(.*)'.*$/
+        lv = File.basename($1)
+        puts "LV: "+lv if @debug
+        check_entry(lv)     
+      end
+    end
+  end
+
+  # Checks found filesystems for backups
+  def check_fs
+    out=""
+    if @testing2
+      # Use testing input (for unit testing)
+      out=@testing2.split("\n")
+    else
+      # Use REAL input
+      # FIXME: Use a more robust way to list filesystems
+      out=`df`
+    end
+    out.each do |line|
+      line.chomp!
+      next if line.match @skip_fs_regex
+      puts "line: #{line}"
+      fs=line.split.last
+      puts "FS: "+fs if @debug
+      check_entry(fs)     
+    end
+  end
+
+  # Checks the given entry (lv or filesystem) to see if we have a backup timestamp
+  def check_entry(entry)
+    expiration=@default_expiration
+    skip=false
+    name=entry.gsub("/","_")
+    if @objects.has_key? entry
+      # Ok, we have an entry, If entry is a hash, read parameters
+      if @objects[entry].instance_of?Hash 
+        expiration=@objects[entry][:expiration] if @objects[entry].has_key? :expiration
+        name=@objects[entry][:name] if @objects[entry].has_key? :name
+      else
+        # If it's not a has, just skip
+        puts "Skipping #{entry}" if @debug
+        skip=true
+      end
+    end
+    unless skip
+      if File.exist? "#{@log_dir}/#{name}"
+        puts "logfile for #{entry} exists, good!" if @debug
+        mtime=File.stat("#{@log_dir}/#{name}").mtime
+        puts "mtime : "+mtime.to_s if @debug
+        if (Time.now - mtime) > expiration
+          puts "OLD backup of #{entry}" if @debug
+          @short+="OLD bcklog for #{entry},"
+          @long+="Backup of #{entry} id too old"
+          incstatus("WARNING")
+        else
+          incstatus("OK")
+        end
+      else
+        puts "  WARNING: no logfile for #{entry}\n" if @debug
+        @short+="no bcklog for #{entry},"
+        @long+="Never seen a backup of #{entry}\n"
+        incstatus("WARNING")
+      end
+    end
+  end
   def info
     %[
 == Description
 
-Checks if we have recent backups of all LVM volumes
-
-FIXME: Only checks LVM, we also need filesystems. CHANGES needed:
-
-0. Add an option skip_all to show that we backup some othr way:
-
-skip_all: Backup of volumes in dom0
-
-1. Before checking /var/lib/pica/obj/lastbackups we will check for /var/lib/modularit/data/BackupChk. If it doesn't exist, fall back to /var/lib/pica/obj/lastbackups.
-2. If it exists, use that one (in YAML)
-3. After checking the LV, check the filesystems. If we configure that a filesistem uses a LV, check using the VG/LV
+Checks if we have recent backups of all LVM volumes and/or filesystems
 
 == Parameters in config file
 
-  :log_dir: Directory for backup timestamps. Default: /var/lib/pica/obj/lastbackups
+  :log_dir: Directory for backup timestamps. Default: /var/lib/modularit/data/lastbackups
   :default_expiration: Default expiration limit in seconds. Default: 4*24*60*60
+  :skip_all: Skip all backup checks and set status OK and return the message specified by this option.
+  :skip_fs_regex: Regex used for filesystems to skip. Default: /^Filesystem|^tmpfs|^rootfs|^devtmpfs/
 
 == Objects format
 
   volume:
     :expiration: expiration time in seconds. 
+    :name: name to look for in log dir. In filesystems we change / for _ (/var -> _var, / -> _) but we can override this whoth the :name options 
 
 If we only specify the volume, just skip that volume from checks
 
@@ -120,6 +148,8 @@ Example:
 dom0_opt:
 dom0_var:
   :expiration: 172800
+/:
+  :name: root
 
 ]    
   end
--- test/test_CheckBackup.rb
+++ test/test_CheckBackup.rb
@@ -3,16 +3,19 @@ require "fileutils"
 
 class TestCheckBackup < Test::Unit::TestCase
 
+  # Check LVM volumes
   context "A CheckBackup on a volume without backups" do
     setup do
       FileUtils.rm Dir.glob('test/CheckBackup/lastbackups/*');
       @check=Rasca::CheckBackup.new("CheckBackup","test/etc",true,true)
       @check.object_dir="test/CheckBackup/nobackups"
-      @check.testing=File.read("test/CheckBackup/output.txt")
+      @check.testing1=File.read("test/CheckBackup/output_lvscan.txt")
+      @check.testing2=""
     end
 
     # FIXME: Should be really CRITICAL and configurable to WARNING
     should 'set status WARNING if volume not skip' do
+      @check.readObjects("CheckBackup")
       @check.check
       assert_equal "WARNING",@check.status
     end
@@ -24,7 +27,6 @@ class TestCheckBackup < Test::Unit::TestCase
     should 'set status OK when we skip all volumes, and show configured messages' do
       @check=Rasca::CheckBackup.new("CheckBackup","test/CheckBackup/etc_skip_backups",true,true)
       @check.object_dir="test/CheckBackup/nobackups"
-      @check.testing=File.read("test/CheckBackup/output.txt")
       @check.check
       assert_equal "OK",@check.status, "Status OK"
       assert_equal "Backups on dom0",@check.short, "Shows right massage"
@@ -38,7 +40,8 @@ class TestCheckBackup < Test::Unit::TestCase
       end
       @check=Rasca::CheckBackup.new("CheckBackup","test/etc",true,true)
       @check.object_dir="test/CheckBackup/old_backups"
-      @check.testing=File.read("test/CheckBackup/output.txt")
+      @check.testing1=File.read("test/CheckBackup/output_lvscan.txt")
+      @check.testing2=""
     end
 
     # FIXME: Should be really CRITICAL and configurable to WARNING
@@ -56,7 +59,72 @@ class TestCheckBackup < Test::Unit::TestCase
       end
       @check=Rasca::CheckBackup.new("CheckBackup","test/etc",true,true)
       @check.object_dir="test/CheckBackup/old_backups"
-      @check.testing=File.read("test/CheckBackup/output.txt")
+      @check.testing1=File.read("test/CheckBackup/output_lvscan.txt")
+      @check.testing2=""
+    end
+
+    should 'set status OK' do
+      @check.check
+      assert_equal "OK",@check.status, "Status is right"
+    end
+  end
+
+  ## Check filesystems
+  context "A CheckBackup on a filesystem without backups" do
+    setup do
+      FileUtils.rm Dir.glob('test/CheckBackup/lastbackups/*');
+      @check=Rasca::CheckBackup.new("CheckBackup","test/etc",true,true)
+      @check.object_dir="test/CheckBackup/nobackups"
+      @check.testing1=""
+      @check.testing2=File.read("test/CheckBackup/output_df.txt")
+    end
+    # FIXME: Should be really CRITICAL and configurable to WARNING
+    should 'set status WARNING if filesystem not skip' do
+      @check.readObjects("CheckBackup")
+      @check.check
+      assert_equal "WARNING",@check.status
+    end
+    should 'set status OK when filesystem is skipped' do
+      @check.object_dir="test/CheckBackup/skipped"
+      @check.check
+      assert_equal "OK",@check.status
+    end
+    should 'set status OK when we skip all filesystem, and show configured messages' do
+      @check=Rasca::CheckBackup.new("CheckBackup","test/CheckBackup/etc_skip_backups",true,true)
+      @check.object_dir="test/CheckBackup/nobackups"
+      @check.check
+      assert_equal "OK",@check.status, "Status OK"
+      assert_equal "Backups on dom0",@check.short, "Shows right massage"
+    end
+  end
+  context "A CheckBackup on a filesystem with OLD backups" do
+    setup do
+      ["_","_var","_boot","_home"].each do |file|
+        FileUtils.touch("test/CheckBackup/lastbackups/#{file}",:mtime=>Time.now-60*60*48)
+      end
+      @check=Rasca::CheckBackup.new("CheckBackup","test/etc",true,true)
+      @check.object_dir="test/CheckBackup/old_backups"
+      @check.testing1=""
+      @check.testing2=File.read("test/CheckBackup/output_df.txt")
+    end
+
+    # FIXME: Should be really CRITICAL and configurable to WARNING
+    should 'set status WARNING if backup is too old' do
+      @check.check
+      assert_equal "WARNING",@check.status, "Status is right"
+      assert_match "OLD",@check.short,"Message reports OLD backup"
+    end
+  end
+
+  context "A CheckBackup on a filesystem with CURRENT backups" do
+    setup do
+      ["root","_var","_boot","_home"].each do |file|
+        FileUtils.touch("test/CheckBackup/lastbackups/#{file}")
+      end
+      @check=Rasca::CheckBackup.new("CheckBackup","test/etc",true,true)
+      @check.object_dir="test/CheckBackup/old_backups"
+      @check.testing1=""
+      @check.testing2=File.read("test/CheckBackup/output_df.txt")
     end
 
     should 'set status OK' do
